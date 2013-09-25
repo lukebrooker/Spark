a81de0ea9a39623c3e7cb469b776040b46400b94
Luke Brooker <mail@lukebrooker.com>
2013-09-25 16:16:08 +1000

    Remove extra javascript left from previous project

--- src/assets/js/components/loader.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $('.loader__container').on('click', '.loader', function(){
-    if ($('.loader__spinner', this).length <= 0) {
-      $('.loader__label', this).append('<span class="loader__spinner"><span></span><span></span><span></span></span>');
-      $(this).addClass('is-active');
-    }
-  });
-});
